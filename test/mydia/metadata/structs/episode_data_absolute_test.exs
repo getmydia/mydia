602823569240a947f06729c6be277e5ab279df89
Alexandre Rosenfeld <arsfeld@gmail.com>
2026-08-17 18:16:07 -0400

    feat(metadata): parse TVDB absoluteNumber and episode id

---
+++ test/mydia/metadata/structs/episode_data_absolute_test.exs
@@ -0,0 +1,40 @@
+defmodule Mydia.Metadata.Structs.EpisodeDataAbsoluteTest do
+  use ExUnit.Case, async: true
+
+  alias Mydia.Metadata.Structs.EpisodeData
+
+  # Shape captured from https://relay.mydia.dev/tvdb/seasons/720595/extended
+  # (Black Clover, official ordering, season 1).
+  defp tvdb_episode do
+    %{
+      "id" => 6_832_458,
+      "seasonNumber" => 1,
+      "number" => 52,
+      "absoluteNumber" => 52,
+      "name" => "Whoever's Strongest Wins",
+      "aired" => "2018-10-02",
+      "runtime" => 25
+    }
+  end
+
+  test "carries absoluteNumber through as absolute_number" do
+    assert %EpisodeData{absolute_number: 52} = EpisodeData.from_tvdb_response(tvdb_episode())
+  end
+
+  test "carries the TVDB episode id through as a string" do
+    assert %EpisodeData{provider_episode_id: "6832458"} =
+             EpisodeData.from_tvdb_response(tvdb_episode())
+  end
+
+  test "tolerates a missing absoluteNumber" do
+    episode = Map.delete(tvdb_episode(), "absoluteNumber")
+
+    assert %EpisodeData{absolute_number: nil, provider_episode_id: "6832458"} =
+             EpisodeData.from_tvdb_response(episode)
+  end
+
+  test "TMDB responses leave both nil" do
+    assert %EpisodeData{absolute_number: nil, provider_episode_id: nil} =
+             EpisodeData.from_api_response(%{"season_number" => 1, "episode_number" => 1})
+  end
+end
